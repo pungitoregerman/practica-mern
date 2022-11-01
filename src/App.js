e46ac2da3edc2a1fcdefe9ad5ae3a90b179d112f
pungitoregerman <pungitoregerman@gmail.com>
2022-11-01 00:06:30 -0300

    task1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
-import NombreFoto from './components/NombreFoto'
+import data from './data/events1';
+import Cards from './components/Cards'
+import CardsComplet from './components/CardsComplet'
+import { useState } from 'react';
+
+function App() {
+  let [numeroAcambiar,setNumeroAcambiar] = useState(0)
+  let [mostrarOcultar,setMostrarOcultar] = useState(false)
+    let next = () =>{
+        if(numeroAcambiar < data.length-1){
+          setNumeroAcambiar(++numeroAcambiar)
+        }else{
+          setNumeroAcambiar(0)
+        }
+      }
+      let back = () =>{
+        if(numeroAcambiar>0){
+          setNumeroAcambiar(--numeroAcambiar)
+        }else{
+          setNumeroAcambiar(data.length -1)
+        }
+      }   
+      let hide = () => {
+        setMostrarOcultar(!mostrarOcultar)
+      }
+
+  return(
+    mostrarOcultar ? ( 
+     <div className='conteiner-card'> 
+    <Cards name= {data[numeroAcambiar].name} image= {data[numeroAcambiar].image} category={data[numeroAcambiar].category} next={next} back={back} hide={hide}/>
+    </div>)
+      : 
+      <div className='conteiner-card'>
+      <CardsComplet name= {data[numeroAcambiar].name} image= {data[numeroAcambiar].image} category ={data[numeroAcambiar].category} description= {data[numeroAcambiar].description} next={next} back={back} hide={hide} />
+      </div>
+    )
+}
+export default App;
+
+//Para usar una variable JS, se emplea directamente con {} 
+
+/* import NombreFoto from './components/NombreFoto'
 import CampoConArray from './components/CampoConArray';
 import Flechita from './components/Flechita';
-import data from './data/datoMentores'
-import { useState } from 'react'
-function App() {
-  
+import data from './data/datoMentores'*/
+
+/*  
   let [mostrarOcultar,setMostrarOcultar] = useState(false)
   let [numeroAcambiar,setNumeroAcambiar] = useState(0)
   //El hook de estado esta compuesto por dos Elementos
@@ -32,8 +72,8 @@ function App() {
   return (
     <div className='card black'>
     <NombreFoto nombre= {data[numeroAcambiar].nombre} foto={data[numeroAcambiar].foto}/>
-    {/*VAMOS A ARMAR UN CONDICIONAL QUE DEPENDA DEL BOOLEANO DEL EVENTO*/}
-    {
+    {/*VAMOS A ARMAR UN CONDICIONAL QUE DEPENDA DEL BOOLEANO DEL EVENTO}*/
+   /* {
       mostrarOcultar ? ( <>
       <p className='gray flex j-center a-center' onClick={hide}>Ocultar info</p>
       <div className='edad-fecha'>
@@ -52,14 +92,10 @@ function App() {
     }
     <div className='flex j-center a-center'>
       <Flechita estilo ='r25' verbo = 'Anterior' onClick ={prev}/>
-      <Flechita estilo ='l25' verbo = 'Siguiente' onClick={next}/>
+      <Flechita estilo ='l25' verbo = 'Siguiente' onClick={next}/>*/
       {/*los eventos en los componentes se pasan como propiedades/metodos NO COMO EVENTOS*/}
       {/*por eso es necesario desestructurarlo adentro del componente*/ }
       {/*y aplciarlo en la etiqueta HTML que corresponda*/}
-    </div>
-  </div>
-  );
-}
-//Para usar una variable JS, se emplea directamente con {} 
-
-export default App;
+   //</div>
+ /* </div>
+  ); */
